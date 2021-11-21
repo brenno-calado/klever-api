f70efbd2d79119d6d261261500f01a53a49abd70
brenno-calado <brenno.calado@gmail.com>
2021-11-21 12:53:25 -0300

    update func name WriteBinToProto

--- serializer/file.go
+++ serializer/file.go
@@ -4,7 +4,9 @@ import (
 	"fmt"
 	"io/ioutil"
 
+	"github.com/golang/protobuf/jsonpb"
 	"google.golang.org/protobuf/proto"
+	"google.golang.org/protobuf/runtime/protoiface"
 )
 
 func WriteProtoToBinFile(message proto.Message, filename string) error {
@@ -32,6 +34,27 @@ func WriteBinToProto(filename string, message proto.Message) error {
 	err = proto.Unmarshal(data, message)
 	if err != nil {
 		return fmt.Errorf("cannot unmarshal binary data to proto message: %w", err)
+	}
+
+	return nil
+}
+
+func WriteProtoToJSONFile(message protoiface.MessageV1, filename string) error {
+	marshaler := jsonpb.Marshaler{
+		EnumsAsInts: false,
+		EmitDefaults: true,
+		Indent: "  ",
+		OrigName: false,
+	}
+
+	data, err := marshaler.MarshalToString(message)
+	if err != nil {
+		return fmt.Errorf("cannot marshal proto message to JSON data: %w", err)
+	}
+
+	err = ioutil.WriteFile(filename, []byte(data), 0644)
+	if err != nil {
+		return fmt.Errorf("cannot write JSON data to file: %w", err)
 	}
 
 	return nil
--- serializer/file_test.go
+++ serializer/file_test.go
@@ -14,6 +14,7 @@ func TestFileSerializer(t *testing.T) {
 	t.Parallel()
 
 	binaryFilename := "../tmp/exchange.bin"
+	jsonFilename := "../tmp/exchange.json"
 
 	exchange1 := sample.NewExchange()
 	err := serializer.WriteProtoToBinFile(exchange1, binaryFilename)
@@ -23,4 +24,7 @@ func TestFileSerializer(t *testing.T) {
 	err = serializer.WriteBinToProto(binaryFilename, exchange2)
 	require.NoError(t, err)
 	require.True(t, proto.Equal(exchange1, exchange2))
+
+	err = serializer.WriteProtoToJSONFile(exchange1, jsonFilename)
+	require.NoError(t, err)
 }
